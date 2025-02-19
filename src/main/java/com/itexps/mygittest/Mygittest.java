66d3be029b9f4a353cf37a3bc2a8512ca8b555fb
tspatel02 <tspatel02@yahoo.com>
2025-02-18 21:29:29 -0600

    my java project

---
+++ src/main/java/com/itexps/mygittest/Mygittest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexps.mygittest;
+
+/**
+ *
+ * @author itexps
+ */
+public class Mygittest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
